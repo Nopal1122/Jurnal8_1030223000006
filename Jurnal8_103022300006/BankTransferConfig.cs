d4c96160d2aeda84aff702c43df090bc5844980c
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 07:01:36 +0700

    Commit pertama

---
+++ Jurnal8_103022300006/BankTransferConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal8_103022300006
+{
+    internal class BankTransferConfig
+    {
+
+    }
+}
